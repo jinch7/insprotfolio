46f47cf4c998fa22e592db4a92d67d47c53c1022
jinch7 <xornjscjf08@gmail.com>
2025-04-12 23:53:23 +0900

    공통 개발 - Exception과 Advice

--- src/main/kotlin/com/insportfolio/portfolio/admin/advice/AdminApiController.kt
+++
@@ -1,6 +0,0 @@
-package com.insportfolio.portfolio.admin.advice
-
-import org.springframework.web.bind.annotation.RestControllerAdvice
-
-@RestControllerAdvice
-class AdminApiController
---
+++ src/main/kotlin/com/insportfolio/portfolio/admin/advice/AdminApiControllerAdvice.kt
@@ -0,0 +1,38 @@
+package com.insportfolio.portfolio.admin.advice
+
+import com.insportfolio.portfolio.admin.exception.AdminException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class AdminApiControllerAdvice {
+    val log = LoggerFactory.getLogger(AdminApiControllerAdvice::class.java)
+
+    @ExceptionHandler
+    fun handlerException(e: AdminException): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        return ResponseEntity.status(e.httpStatus).body(e.message)
+    }
+
+    @ExceptionHandler
+    fun handlerException(e: MethodArgumentNotValidException): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        val fieldError = e.bindingResult.fieldErrors[0]
+        val message = "[${fieldError.field} ${fieldError.defaultMessage}]"
+
+        return ResponseEntity.badRequest().body(message)
+    }
+
+    @ExceptionHandler
+    fun handlerException(e: Exception): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        return ResponseEntity.internalServerError().body("[system error]")
+    }
+}
