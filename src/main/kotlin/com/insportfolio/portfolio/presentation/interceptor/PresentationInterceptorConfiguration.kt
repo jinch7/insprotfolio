a816950cbb8ef9fbac57d3eef105e7967e473fe9
jinch7 <xornjscjf08@gmail.com>
2025-04-12 22:35:42 +0900

    인터셉터 개발

--- src/main/kotlin/com/insportfolio/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/insportfolio/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -1,6 +1,19 @@
 package com.insportfolio.portfolio.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration
+class PresentationInterceptorConfiguration(
+    private val presentationaInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationaInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns(
+                "/assets/**", "/css/**", "/js/**", "/admin/**", "h2**",
+                "/favicon.ico", "/error"
+            )
+    }
+}
